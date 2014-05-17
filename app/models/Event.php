7830761c170f7a7f6a2f2f9fbdb47cbb3fe17f47
Joe Anzalone <Joe@Shmit.com>
2014-05-17 12:04:15 -0400

    Render Event descriptions as HTML using Markdown class

--- app/models/Event.php
+++ app/models/Event.php
@@ -1,10 +1,17 @@
 <?php
 
+use \Michelf\Markdown;
+
 class Event extends \Eloquent {
     protected $fillable = [];
 
     public function guests()
     {
         return $this->hasMany('Guest');
+    }
+
+    public function getDescriptionAttribute($description)
+    {
+        return Markdown::defaultTransform($description);
     }
 }
