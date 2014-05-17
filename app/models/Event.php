755bbea54fbe6bb5c93a368fc9059917ec7f0b7a
Joe Anzalone <Joe@Shmit.com>
2014-05-17 13:37:20 -0400

    Concatenate the start date and time into one property when instantiating Event object

--- app/models/Event.php
+++ app/models/Event.php
@@ -5,6 +5,15 @@ use \Michelf\Markdown;
 class Event extends \Eloquent {
     protected $fillable = ['title', 'description', 'location', 'start_time', 'end_time'];
 
+    public function __construct($params = [])
+    {
+        if (isset($params['start_time_date']) && isset($params['start_time_time'])) {
+            $params['start_time'] = $params['start_time_date'] . ' ' . $params['start_time_time'];
+        }
+
+        return parent::__construct($params);
+    }
+
     public function guests()
     {
         return $this->hasMany('Guest');
--- app/views/events/create.blade.php
+++ app/views/events/create.blade.php
@@ -2,7 +2,7 @@
     <label>Title{{ Form::text('title', '', ['placeholder' => 'Title']) }}</label>
     <label>Description{{ Form::textarea('description', '', ['placeholder' => 'Description']) }}</label>
     <label>Location{{ Form::text('location', '', ['placeholder' => 'Location']) }}</label>
-    <label>Start Date{{ Form::input('date', 'start_date', '', ['placeholder' => 'Location']) }}</label>
-    <label>Start Time{{ Form::input('time', 'start_time', '', ['placeholder' => 'Location']) }}</label>
+    <label>Start Date{{ Form::input('date', 'start_time_date', '', ['placeholder' => 'Location']) }}</label>
+    <label>Start Time{{ Form::input('time', 'start_time_time', '', ['placeholder' => 'Location']) }}</label>
     {{ Form::submit(); }}
 {{ Form::close() }}
