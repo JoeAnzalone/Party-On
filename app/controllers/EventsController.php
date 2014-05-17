5c56c2087e912d346e33e28657e24e9a652c9273
Joe Anzalone <Joe@Shmit.com>
2014-05-17 11:43:39 -0400

    Convert indenting to spaces in controllers

--- app/controllers/EventsController.php
+++ app/controllers/EventsController.php
@@ -2,84 +2,84 @@
 
 class EventsController extends \BaseController {
 
-	protected $layout = 'layouts.main';
+    protected $layout = 'layouts.main';
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$events = Event::all();
-		$this->layout->content = View::make('events.index', ['events' => $events]);
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $events = Event::all();
+        $this->layout->content = View::make('events.index', ['events' => $events]);
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($key, $slug = '')
-	{
-		$guest = Guest::where('key', $key)->firstOrFail();
-		$event = $guest->event;
-		$data = $event;
-		$this->layout->content = View::make('events.show', $data);
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($key, $slug = '')
+    {
+        $guest = Guest::where('key', $key)->firstOrFail();
+        $event = $guest->event;
+        $data = $event;
+        $this->layout->content = View::make('events.show', $data);
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
