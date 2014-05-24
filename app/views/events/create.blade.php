e8fc0648a866a20b706e733e7b7df65e25ae1245
Joe Anzalone <Joe@Shmit.com>
2014-05-24 10:12:05 -0400

    Modify event creation/edit form to support the new location_name and location_address fields

--- app/views/events/create.blade.php
+++ app/views/events/create.blade.php
@@ -2,7 +2,8 @@
     <ul class="fields">
         <li><label for="title">Title</label>{{ Form::text('title', null, ['placeholder' => 'Title', 'id' => 'title']) }}</li>
         <li><label for="description">Description</label>{{ Form::textarea('description', null, ['placeholder' => 'Description', 'id' => 'description']) }}</li>
-        <li><label for="location">Location</label>{{ Form::text('location', null, ['placeholder' => 'Location', 'id' => 'location']) }}</li>
+        <li><label for="location_name">Location Name</label>{{ Form::text('location_name', null, ['placeholder' => 'Location name', 'id' => 'location_name']) }}</li>
+        <li><label for="location_address">Location Address</label>{{ Form::text('location_address', null, ['placeholder' => 'Location address', 'id' => 'location_address']) }}</li>
         <li><label for="start_time_date">Start Date</label>{{ Form::input('date', 'start_time_date', null, ['placeholder' => 'Location', 'id' => 'start_time_date']) }}</li>
         <li><label for="start_time_time">Start Time</label>{{ Form::input('time', 'start_time_time', null, ['placeholder' => 'Location', 'id' => 'start_time_time']) }}</li>
         <li><label for="guests_string">Guests</label>{{ Form::textarea('guests_string', null, ['placeholder' => 'Guests', 'id' => 'guests_string']) }}</li>
